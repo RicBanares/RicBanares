37d922c0ee1c13e14262749a64b72b78a147f30a
RicBanares <94274193+RicBanares@users.noreply.github.com>
2021-11-14 12:43:04 +0800

    QLess API
    QLess API code snippet

---
+++ RegularCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Card
+    {
+        public int CardId { get; set; }
+        public string CreatedDate { get; set; }
+        public string UpdatedDate { get; set; }
+        public int InitialLoad { get; set; }
+        public int CurrentLoad { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CardType { get; set; }
+        public string IDNumber { get; set; }
+    }
+}
